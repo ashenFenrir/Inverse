7702af025724bbab565579c03796b4db085aa014
InfoClassAshenFenrir <shiroi.fenrir@gmail.com>
2024-06-05 13:23:00 +0300

    init: third commit

---
+++ vent.gd
@@ -0,0 +1,27 @@
+extends StaticBody2D
+@onready  var CollisionShape: CollisionShape2D = $CollisionShape2D as CollisionShape2D
+
+var fliyng_objects: Array = []
+
+func enable():
+	CollisionShape.set_deferred("disabled", true)
+func disable():
+	CollisionShape.set_deferred("disabled", false)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+@export var force: float = 0.5
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	for i in fliyng_objects:
+		i.apply_force(Vector2(force, force), Vector2.UP)
+
+
+func _on_area_2d_body_entered(body):
+	fliyng_objects.append(body)
+
+
+func _on_area_2d_body_exited(body):
+	fliyng_objects.erase(body)
